169ad37432b403abee17a9ff1ab25f714ae36605
Felipe <feload@gmail.com>
2018-06-21 09:14:14 -0500

    Add naviationOptions property

--- App.js
+++ App.js
@@ -11,15 +11,38 @@ import ReviewScreen from './screens/ReviewScreen';
 const Navigator = createBottomTabNavigator({
   auth: AuthScreen,
   welcome: WelcomeScreen,
-  main: createBottomTabNavigator({
-    map: MapScreen,
-    deck: DeckScreen,
-    review: createStackNavigator({
-      settings: SettingsScreen,
-      review: ReviewScreen
+  main: {
+    navigationOptions: {
+      title: 'Main'
+    },
+    screen: createBottomTabNavigator({
+      map: MapScreen,
+      deck: DeckScreen,
+      review: {
+        navigationOptions: {
+          title: 'Review Jobs'
+        },
+        screen: createStackNavigator({
+          review: ReviewScreen,
+          settings: SettingsScreen
+        })
+      }
     })
-  })
+  }
 });
+
+/**
+createStackNavigator({
+  settings: SettingsScreen,
+  review:  {
+    screen: ReviewScreen,
+    navigationOptions: () => ({
+      title: `A`,
+      headerBackTitle: null
+    })
+  }
+})
+*/
 
 export default class App extends React.Component {
   render () {
